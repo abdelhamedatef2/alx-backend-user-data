986d8213ec2d113be73c3e72c60e909d6241ae06
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2024-01-23 15:54:13 +0200

    add auto increment to id

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-"""
-User model
-"""
+""" module for user model """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """User model
-
-    Args:
-        Base (class): declarative base from sqlalchemy
-    """
+    """ user model table by sqlalchemy """
     __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
